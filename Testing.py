3e05be004ec1a30bdbed0fe3864746bbe8c34a72
Mithun Suresh <mthnsrsh@gmail.com>
2020-07-22 14:55:54 +0400

    Base

--- Testing.py
+++ Testing.py
@@ -1,18 +1,21 @@
-import pygame
-pygame.init()
-dis=pygame.display.set_mode((400,300))
- 
-pygame.display.set_caption('Snake game ')
- 
-blue=(0,0,255)
-red=(255,0,0)
- 
-game_over=False
-while not game_over:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game_over=True
-    pygame.draw.rect(dis,blue,[200,150,10,10])
-    pygame.display.update()
-pygame.quit()
-quit()
+#import time
+
+import time
+
+#intro classic
+name = input ("What is your name?")
+print("Hello, " + name, "Time to play hangman!")
+Print("")
+
+#sleep
+time.sleep(1)
+
+print("Start guessing...")
+time.sleep(0.5)
+
+#set your keyword
+word = "secret"
+
+# creats a variable with empty value
+guesses =''
+
